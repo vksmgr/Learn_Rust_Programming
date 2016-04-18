08ab77d2e1e2d78e72ba432c31cb8a2c16ca6433
magar_vikas.ghrcecs@raisoni.net <magar_vikas.ghrcecs@raisoni.net>
2016-04-18 18:27:45 +0530

    Today We learn About Enum

---
+++ src/learning.rs
@@ -0,0 +1,32 @@
+//This module is for learning the rust programming in which we are going to learn basic concepts of rust programming language.
+#[derive(Debug,Clone, Copy)]
+pub enum Name{
+    Vikas,
+    Shyam,
+    Ram,
+}
+
+pub fn Hello(){
+    let vikas = Name::Vikas;
+    let shyam = Name::Shyam;
+    let ram = Name::Ram;
+    print_name(vikas);
+}
+pub fn print_name(name: Name){
+    //use Name::*;
+        match name {
+        Name::Vikas => hello_vikas(),
+        Name::Shyam => hello_Shyam(),
+        Name::Ram => hello_ram(),
+    };
+}
+fn hello_vikas(){
+    println!("hello ");
+}
+
+fn hello_Shyam(){
+    println!("hello shyam");
+}
+fn hello_ram(){
+    println!("hello Ram");
+}
